fb545d9829cf0d5a6d4b82e3517b7283d182f466
yaho <yaser.hoseini86@gmail.com>
2023-10-16 12:37:45 +0330

    add posts to page and set env file

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,20 @@
 // 'use client'
-// import { getPost } from '@/lib/fetchData'
+import { getPost } from '@/lib/fetchData'
 
 
 export default async function Home() {
-  // const posts = await getPost()
+  const posts = await getPost()
   return (
    <>
    <div>
-    {/* {
+    {
       posts.map(pt => <>
        <h1>{pt.title}</h1>
        <p>{pt.body}</p>
       </>
         )
-    } */}
-    hi there
+    }
+    
    </div>
    </>
   )
