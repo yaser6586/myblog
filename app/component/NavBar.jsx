3c269000e4655c072d3196263c07a7d56d26b1d1
yaho <yaser.hoseini86@gmail.com>
2023-10-16 13:16:07 +0330

    change Link to a

--- app/component/NavBar.jsx
+++ app/component/NavBar.jsx
@@ -27,7 +27,7 @@ function Navbar() {
             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
           >
             <li>
-              <Link href="/">home</Link>
+              <a href="/">home</a>
             </li>
 
             <li>
@@ -40,7 +40,7 @@ function Navbar() {
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <Link href="/">home</Link>
+            <a href="/">home</a>
           </li>
 
           <li>
