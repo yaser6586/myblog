25b579b6fab0b39466588567ec0934c6786a17b6
yaho <yaser.hoseini86@gmail.com>
2023-10-18 18:05:09 +0330

    some eslint error has been corrected

--- app/component/SideBar.jsx
+++ app/component/SideBar.jsx
@@ -12,14 +12,14 @@ function SideBar() {
         alt="my pic"
       />
       <p className="text-center mx-10 mt-8 ">
-        My name is <b>Yaser Hoseini</b> I'm computer engineer and my main skill
-        is front end development, this weblog is my Portfolio . this weblog is
+        My name is <b>Yaser Hoseini</b>Im computer engineer and my main skill is
+        front end development, this weblog is my Portfolio . this weblog is
         fullstack app and developed by me with Next js and is connected to
         mongodb database and you can get blog page to add new post to weblog
         after that in home page see the post by clicking read button you
         redirect to dynamic page which is contained the post detail like body of
         post , you will be able to edit and delete each post in detail page .
-        I've added REST api to app and deploy with Vercel you can toggle between
+        Ive added REST api to app and deploy with Vercel you can toggle between
         dark and light theme in navbar which use the Context api . this web site
         use DaisyUi and Tailwind for styling and better appearance its
         completely responsive , last but not least ,this website has server side
